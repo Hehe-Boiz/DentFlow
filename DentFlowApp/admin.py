56831cc6dbe8f18b30227593225a1a16507fdf4e
BaTannnn <nguyenbatan.2908@gmail.com>
2025-12-03 03:51:23 +0700

    Init base project

---
+++ DentFlowApp/admin.py
@@ -0,0 +1,37 @@
+from flask import redirect
+from flask_admin.contrib.sqla import ModelView
+from flask_admin import Admin, BaseView, expose
+from DentFlowApp import app,db
+from flask_login import current_user,logout_user
+from DentFlowApp.models import UserRole,LichHen,BacSi,Thuoc,HoSoKhamBenh
+
+admin = Admin(app=app, name="DentFlow")
+
+class ReceptionistModelView(ModelView):
+    def is_accessible(self) -> bool:
+        return current_user.is_authenticated and current_user.user_role == UserRole.RECEPTIONIST
+
+class AdminModelView(ModelView):
+    def is_accessible(self) -> bool:
+        return current_user.is_authenticated and current_user.user_role == UserRole.ADMIN
+
+class CashierModelView(ModelView):
+    def is_accessible(self) -> bool:
+        return current_user.is_authenticated and current_user.user_role == UserRole.CASHIER
+
+
+class LogoutView(BaseView):
+    @expose('/')
+    def index(self):
+        logout_user()
+        return redirect('/admin')
+
+    def is_accessible(self) -> bool:
+        return current_user.is_authenticated
+
+
+
+admin.add_view(ReceptionistModelView(HoSoKhamBenh, db.session, name="Quản lý hồ sơ"))
+admin.add_view(ReceptionistModelView(LichHen, db.session, name="Quản lý Lịch Hẹn"))
+admin.add_view(AdminModelView(Thuoc,db.session, name="Quản lý thuốc"))
+admin.add_view(LogoutView(name="Đăng xuất"))
---
+++ DentFlowApp/index.py
@@ -0,0 +1,37 @@
+from flask_admin import BaseView, expose
+
+from DentFlowApp import app
+from flask import render_template, request,redirect
+from flask_login import login_user, logout_user
+from DentFlowApp import dao,login
+
+@app.route("/")
+def hello_world():
+    return "Hello World!!!"
+
+
+@app.route('/login', methods=['post'])
+def login_process():
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    u = dao.auth_user(username=username, password=password)
+    if u:
+        login_user(user=u)
+
+    next = request.args.get('next')
+    return redirect(next if next else '/')
+
+@app.route('/logout')
+def logout_process():
+    logout_user()
+    return redirect('/login')
+
+@login.user_loader
+def load_user(user_id):
+    return dao.get_user_by_id(user_id)
+
+if __name__ == "__main__":
+    from DentFlowApp import admin
+
+    app.run()
