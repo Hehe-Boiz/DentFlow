f5c3d7ecae56bcf0e6bcabed736d59671049fef3
khaquachtrong <khaquachtrong74@gmail.com>
2025-12-21 05:18:22 +0700

    feat: add view history cashier and fix logic search phieu dieu tri

--- DentFlowApp/apis/cashier_apis.py
+++ DentFlowApp/apis/cashier_apis.py
@@ -5,7 +5,8 @@ from flask import render_template, request
 from flask_login import current_user, login_required
 
 from DentFlowApp.dao.bacsi_dao import get_doctors_by_id
-from DentFlowApp.dao.thungan_dao import get_phieu_dieu_tri_chua_thanh_toan, get_dich_vu_by_id
+from DentFlowApp.dao.thungan_dao import get_phieu_dieu_tri_chua_thanh_toan, get_dich_vu_by_id, \
+    get_ds_phieu_dieu_tri_da_thanh_toan
 from DentFlowApp.models import UserRole
 
 
@@ -14,8 +15,12 @@ from DentFlowApp.models import UserRole
 def cashier_view():
     if current_user.is_authenticated and current_user.vai_tro == UserRole.CASHIER:
         page = request.args.get('page', 1, type=int)
-        phieu_dieu_tri_s = get_phieu_dieu_tri_chua_thanh_toan(page=page)  # pagination
-        return render_template('cashier/cashier.html', thungan=True, pagination=phieu_dieu_tri_s)
+        active_tab = request.args.get('tab')
+        ds_phieu_dieu_tri_chua_thanh_toan = get_phieu_dieu_tri_chua_thanh_toan(page=page)  # pagination
+        ds_phieu_dieu_tri_da_thanh_toan = get_ds_phieu_dieu_tri_da_thanh_toan(page=page)
+        return render_template('cashier/cashier.html', thungan=True, pagination=ds_phieu_dieu_tri_chua_thanh_toan,
+                               active_tab=active_tab,
+                               pagination_tt=ds_phieu_dieu_tri_da_thanh_toan)
     return http.HTTPStatus.FORBIDDEN
 
 
--- DentFlowApp/dao/thungan_dao.py
+++ DentFlowApp/dao/thungan_dao.py
@@ -10,3 +10,15 @@ def get_phieu_dieu_tri_chua_thanh_toan(page=1):
 
 def get_dich_vu_by_id(id_dich_vu):
     return DichVu.query.get(id_dich_vu)
+
+
+def get_phieu_dieu_tri_da_thanh_toan_by_id(id):
+    return PhieuDieuTri.query.filter(
+        PhieuDieuTri.id == id and PhieuDieuTri.trang_thai_thanh_toan == TrangThaiThanhToan.DA_THANH_TOAN).first()
+
+
+def get_ds_phieu_dieu_tri_da_thanh_toan(page=1):
+    query = PhieuDieuTri.query
+    query = query.filter(PhieuDieuTri.trang_thai_thanh_toan == TrangThaiThanhToan.DA_THANH_TOAN)
+    query = query.order_by(PhieuDieuTri.ngay_tao)
+    return query.paginate(page=page, per_page=20, error_out=False)
