a10a7990914f82f5d4f0a3a65eca33e3da4f8b7f
HeheBoiz <leminhnhut155@gmail.com>
2025-12-21 21:42:16 +0700

    update

--- DentFlowApp/static/js/doctor.js
+++ DentFlowApp/static/js/doctor.js
@@ -29,7 +29,43 @@ buttons.forEach(btn => {
 
         tabEl.innerHTML = "<p class='text-sm text-gray-500'>Đang tải...</p>";
 
-        const res = await fetch(url, { cache: "no-store" });
+        const res = await fetch(url, {cache: "no-store"});
         tabEl.innerHTML = await res.text();
     });
+});
+
+const STATUS_MAP = {
+    "Đã khám": {
+        text: "Đã khám",
+        classes: ["bg-green-100", "text-green-700"],
+        icon: "fa-regular fa-circle-check fa-lg mt-2-5 w-3 h-3 mr-2"
+    },
+    "Chờ khám": {
+        text: "Chờ khám",
+        classes: ["bg-yellow-100", "text-yellow-700"],
+        icon: "fa-regular fa-clock fa-lg mt-2-5 w-3 h-3 mr-2"
+    },
+};
+
+
+const badges = document.querySelectorAll(".trang-thai-lich-hen");
+
+badges.forEach(span => {
+    // Lấy giá trị từ data-trang-thai (được render từ Jinja2)
+    const statusKey = span.getAttribute("data-trang-thai");
+    console.log(statusKey)
+    // Tìm cấu hình trong Map
+    const config = STATUS_MAP[statusKey];
+
+    if (config) {
+        span.innerHTML =""
+        // Xóa các class màu mặc định (để tránh bị trùng lặp)
+        span.classList.remove(
+                "bg-green-100", "text-green-700",
+                "bg-yellow-100", "text-yellow-700",
+            );
+        span.classList.add(...config.classes);
+
+        span.innerHTML = `<i class="${config.icon}"></i>${config.text}`;
+    }
 });
